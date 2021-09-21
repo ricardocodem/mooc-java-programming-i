adcbd150f0e9d4b2ff39946214bb3d3478640b95
ricardocodem <52762615+ricardocodem@users.noreply.github.com>
2021-09-21 18:03:29 -0300

    Initial commit

---
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -0,0 +1,28 @@
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class RecordsFromAFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //get file name
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+
+        //task to read file and extract data
+        try (Scanner fileName = new Scanner(Paths.get(file))) {
+            while (fileName.hasNextLine()) {
+                String line = fileName.nextLine();
+
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+
+                System.out.println(name + ", age: " + age + " years");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
