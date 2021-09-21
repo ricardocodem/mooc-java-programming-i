88b14b149dc1b526e0417589ee1fe78602e1960a
ricardocodem <52762615+ricardocodem@users.noreply.github.com>
2021-09-21 18:26:30 -0300

    Initial commit

---
+++ part05-Part05_14.Books/src/main/java/Book.java
@@ -0,0 +1,39 @@
+
+public class Book {
+
+    private String name;
+    private int publicationYear;
+
+    public Book(String name, int publicationYear) {
+        this.name = name;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    @Override
+    public boolean equals(Object compared) {
+        // if the variables are located in the same position, they are equal
+        if (this == compared) {
+            return true;
+        }
+
+        // if the compared object is not of type Book, the objects are not equal
+        if (!(compared instanceof Book)) {
+            return false;
+        }
+
+        // convert the object to a Book object
+        Book comparedBook = (Book) compared;
+
+        // if the values of the object variables are equal, the objects are, too
+        return this.name.equals(comparedBook.name) && this.publicationYear == (comparedBook.publicationYear);
+    }
+
+}
