1dce0166dc6776dea566cedb8c7b6cdc18497944
ricardocodem <52762615+ricardocodem@users.noreply.github.com>
2021-09-21 18:18:44 -0300

    Initial commit

---
+++ part05-Part05_07.NullPointerException/src/main/java/Line.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ricardo
+ */
+public class Line {
+    private String texto;
+
+    public Line(String a){
+        this.texto = a;
+    }     
+    public String getTexto(){
+        return texto;
+    }
+}
+
