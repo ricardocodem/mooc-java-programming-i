6ba7b93ac8e874a35915407b6fec6a1b69fcc10b
ricardocodem <52762615+ricardocodem@users.noreply.github.com>
2021-09-21 13:31:45 -0300

    Initial commit

---
+++ part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -0,0 +1,21 @@
+
+public class Printer {
+
+    public static void main(String[] args) {
+        // You can test the method here
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
+    }
+
+    public static void printArrayInStars(int[] array) {
+        // Write some code in here
+        int star = 0;
+        for (int index : array) {
+            star = index;
+            for (int i = 0; i < star; i++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
