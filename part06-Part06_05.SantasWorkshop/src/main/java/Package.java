4c6c11da5b4bfe1a65c4a424b77b9708b358a945
ricardocodem <52762615+ricardocodem@users.noreply.github.com>
2021-09-21 18:32:56 -0300

    Initial commit

---
+++ part06-Part06_05.SantasWorkshop/src/main/java/Package.java
@@ -0,0 +1,32 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Ricardo
+ */
+public class Package {
+
+    private ArrayList<Gift> gifts;
+
+    public Package() {
+        this.gifts = new ArrayList<>();
+    }
+
+    public void addGift(Gift gift) {
+        gifts.add(gift);
+    }
+
+    public int totalWeight() {
+        int sumWeight = 0;
+        for (Gift weight : gifts) {
+            sumWeight+= weight.getWeight();
+        }
+        return sumWeight;
+    }
+}
